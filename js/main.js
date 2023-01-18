ab874e3bec9b6d26b0d688ac1d9f71ae5d73e4cf
Franco Marsili <92090620+FrannMar@users.noreply.github.com>
2023-01-18 21:01:14 +0100

    cambios nuevos

--- js/main.js
+++ js/main.js
@@ -7,12 +7,10 @@ let total = 0;
 function eleccion(){
     let option = prompt('Bienvenido a Yazluc Accesorios, Seleccione la opción que buscas: \n ' + 
     '1- Aros. \n2- Collares. \n3 Accesorios Celu. \n4 Accesorios PC. \n5 Otros. \n0- Salir.');
-    // validar con while que option este vacio
     while(option == null || option == ''){
         option = prompt ('Ingrese una opción válida por favor: \n ' +
         '1- Aros. \n2- Collares. \n3 Accesorios Celu. \n4 Accesorios PC. \n5 Otros. \n0- Salir.');
     }
-    // validar con while que option sea 1, 2, 3, 4, 5 o 0
     while(option != '1' && option != '2' && option != '3' && option != '4' && option != '5' && option != '0'){
         option = prompt('Ingrese una opcion válida por favor: \n ' +
         '1- Aros. \n2- Collares. \n3 Accesorios Celu. \n4 Accesorios PC. \n5 Otros. \n0- Salir.');
@@ -29,25 +27,25 @@ function procesoDeEleccion(option){
         case '1':
             alert ("El precio de los aros es: " + precioaros);
             let cantidadaros = prompt("¿Cuántos aros desea comprar?");
-            let totalaros = totalFinal1(precioaros, cantidadaros);
+            let totalaros = totalFinal(precioaros, cantidadaros);
             alert("El total a pagar es: " + totalaros);
             break;
         case '2':
             alert ("El precio de los collares es: " + preciocollares)
             let cantidadcollares = prompt("¿Cuántos collares desea comprar?");
-            let totalcollares = totalFinal2(preciocollares, cantidadcollares);
+            let totalcollares = totalFinal(preciocollares, cantidadcollares);
             alert("El total a pagar es: " + totalcollares);
             break;
         case '3':
             alert ("El precio de los accesorios para celulares es: " + preciocelulares);
             let cantidadcelulares = prompt("¿Cuántos accesorios de celular desea comprar?");
-            let totalcelulares = totalFinal3(preciocelulares, cantidadcelulares);
+            let totalcelulares = totalFinal(preciocelulares, cantidadcelulares);
             alert("El total a pagar es: " + totalcelulares);
             break;
         case '4':
             alert ("El precio de los accesorios para PC es: " + preciopc);
             let cantidadpc = prompt("¿Cuántos artículos de PC desea comprar?");
-            let totalpc = totalFinal4(preciopc, cantidadpc);
+            let totalpc = totalFinal(preciopc, cantidadpc);
             alert("El total a pagar es: " + totalpc);
             break;
         case '5':
@@ -55,24 +53,79 @@ function procesoDeEleccion(option){
             break;
     }
 }
-function totalFinal1(precioaros, cantidadaros){
+function totalFinal(precioaros, cantidadaros){
     totalaros = precioaros * cantidadaros;
     return totalaros;
 }
-function totalFinal2(preciocollares, cantidadcollares){
+function totalFinal(preciocollares, cantidadcollares){
     totalcollares = preciocollares * cantidadcollares;
     return totalcollares;
 }
-function totalFinal3(preciocelulares, cantidadcelulares){
+function totalFinal(preciocelulares, cantidadcelulares){
     totalcelulares = preciocelulares * cantidadcelulares;
     return totalcelulares;
 }
-function totalFinal4(preciopc, cantidadpc){
+function totalFinal(preciopc, cantidadpc){
     totalpc = preciopc * cantidadpc;
     return totalpc;
 }
-prompt('¿Cuántos aros llevas?');
+prompt('Hay una promo del 20% y 30% en aros ¿Cuántos aros llevas?');
 let descuento20 = precioaros - (precioaros  * 0.2);
 let descuento30 = precioaros - (precioaros  * 0.3);
-alert("el precio por unidad con descuento del 20% es de " + descuento20);
-alert("el precio por unidad con descuento del 30% es de " + descuento30);
+alert("el precio por unidad de aros con descuento del 20% es de " + descuento20);
+alert("el precio por unidad de aros con descuento del 30% es de " + descuento30);
+//DATE
+const hoy = new Date("January 18, 2023");
+const findemes = new Date("March 1, 2023");
+const milisegundospordia = 86400000;
+alert ("Nuestra oferta en aros finaliza en: " + ((findemes - hoy)/ milisegundospordia ) + "dias");
+//OBJETOS
+const productoAros = [{id:1, producto:"cruz", seccion: "Aros", precio: 500},
+                {id:2, producto: "classic", seccion: "Aros", precio: 500},
+                {id: 3, producto: "estrellita", seccion: "Aros", precio: 500 },
+                {id: 4, producto: "finos", seccion: "Aros", precio: 500}]
+console.log (productoAros);
+//
+const productoColgantes = [{id:5, producto: "Varios", seccion: "Colgantes", precio: 700}];
+console.log(productoColgantes);
+//
+const productoCelulares = [{id:6, producto: "variosCelu", seccion:"Celulares", precio: 700},
+                    {id:7, producto: "S10+Auris", seccion: "Celulares", precio: 700},
+                    {id:8, producto: "genAuris", seccion: "Celulares", precio: 700},
+                    {id:9, producto:"cableTipoC", seccion: "Celulares", precio: 700},
+                    {id:10, producto: "vidrio", seccion: "Celulares", precio: 700}]
+console.log(productoCelulares);
+//
+const productoPC = [{id:11, producto:"joystick", seccion: "PC", precio: 1200},
+{id:12, producto: "antena", seccion: "PC", precio: 1000},
+{id: 13, producto: "kingston", seccion: "PC", precio: 300 },
+{id: 14, producto: "usb5.0", seccion: "PC", precio: 500}]
+console.log(productoPC);
+const productos = productoAros.concat(productoColgantes, productoCelulares, productoPC);
+console.log(productos);
+//
+function Tengo (b) {
+    return (a) => a > b;
+    //14 => 14 > 50;
+}
+let yoDeberia = Tengo (14) ;
+console.log( yoDeberia(14)) ; //lo que debo tener cuando coloque todos los productos;
+console.log( yoDeberia(50) ) ; //lo que debo tener cuando coloque todos los productos;
+//fiter para filtrar por precio igual o menor a 500 pesos;
+const filtroPrecio = productos.filter( (el) => el.precio <= 500);
+console.log(filtroPrecio);
+// precios ordenados de menor a mayor;
+const precio = [500,700,1200,1000,300]
+precio.sort ( (a, b) => a - b);
+alert("nuestros precios de menor a mayor son:" + precio);
+console.log (precio);
+// precios ordenados de mayor a menor;
+precio.sort ( (a, b) => b - a);
+alert("nuestros precios de mayor a menor son:" + precio);
+console.log (precio);
+//
+alert("El precio del producto mas caro de la web es de " + Math.max (500,700,1200,1000,300));
+console.log(Math.max (500,700,1200,1000,300));
+//
+alert("El precio del producto mas barato de la web es de " + Math.min (500,700,1200,1000,300));
+console.log(Math.min (500,700,1200,1000,300));
